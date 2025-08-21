632bee1fa470cec863fa71ec9170755864dcce76
Karol Angrys <karol.angrys@gmail.com>
2025-08-21 21:09:27 +0200

    fix: Fix

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,27 +1,30 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'en',
-    debug: process.env.NODE_ENV === 'development',
-    
+    fallbackLng: "en",
+    debug: process.env.NODE_ENV === "development",
+
     interpolation: {
       escapeValue: false,
     },
-    
+
     backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json',
+      loadPath:
+        process.env.NODE_ENV === "production"
+          ? "/TeamRandomizer/"
+          : "" + "/locales/{{lng}}/{{ns}}.json",
     },
-    
+
     detection: {
-      order: ['localStorage', 'navigator', 'htmlTag'],
-      caches: ['localStorage'],
+      order: ["localStorage", "navigator", "htmlTag"],
+      caches: ["localStorage"],
     },
   });
 
